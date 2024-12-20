2507f557b7bf99b0b44696a51a3bc4e0ff450b93
Sam Muttai <muttaisammy@gmail.com>
2024-12-20 13:42:49 +0300

    final

---
+++ Database/developers.sql
@@ -0,0 +1,394 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2020 at 11:34 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `developers`
+--
+CREATE DATABASE IF NOT EXISTS `developers` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `developers`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+DROP TABLE IF EXISTS `about`;
+CREATE TABLE `about` (
+  `id` int(10) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` longtext NOT NULL,
+  `image` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id`, `title`, `content`, `image`) VALUES
+(10, 'About Our Company', '"We are a Real Estate company in Kenya. List all types of properties for Sale, Lease & Rent ', 'video.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `aid` int(10) NOT NULL,
+  `auser` varchar(50) NOT NULL,
+  `aemail` varchar(50) NOT NULL,
+  `apass` varchar(50) NOT NULL,
+  `adob` date NOT NULL,
+  `aphone` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`aid`, `auser`, `aemail`, `apass`, `adob`, `aphone`) VALUES
+(2, 'disha', 'disha@gmail.com', 'disha', '1999-02-02', '9689689698'),
+(6, 'final', 'final@gmail.com', 'final', '2020-04-29', '7979656578'),
+(7, 'test', 'test@gmail.com', 'test', '2020-04-29', '8997979765'),
+(8, 'check', 'check@gmail.com', 'check', '2020-04-29', '8979785688'),
+(9, 'admin', 'admin@gmail.com', 'admin', '1999-12-06', '9878786545');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city` (
+  `cid` int(50) NOT NULL,
+  `cname` varchar(100) NOT NULL,
+  `sid` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`cid`, `cname`, `sid`) VALUES (1, 'Mombasa',1), (2, 'Kwale',2), (3, 'Kilifi',3), (4, 'Hola',4), (5, 'Lamu',5), (6, 'Mwatate',6), (7, 'Garissa',7), (8, 'Wajir',8), (9, 'Mandera',9), (10, 'Marsabit',10), (11, 'Isiolo',11), (12, 'Meru',12), (13, 'Kathwana',13), (14, 'Embu',14), (15, 'Kitui',15), (16, 'Machakos',16), (17, 'Wote',17), (18, 'Ol kalau',18), (19, 'Nyeri',19), (20, 'Kerugoya/Kutus',20), (21, "Murang'a",21), (22, 'Kiambu',22), (23, 'Lodwar',23), (24, 'Kapenguria',24), (25, 'Maralal',25), (26, 'Kitale',26), (27, 'Eldoret',27), (28, 'Iten',28), (29, 'Kapsabet',29), (30, 'Kabarnet',30), (31, 'Nanyuki',31), (32, 'Nakuru',32), (33, 'Narok',33), (34, 'Kajiado',34), (35, 'Kericho',35), (36, 'Bomet',36), (37, 'Kakamega',37), (38, 'Vihiga',38), (39, 'Bungoma',39), (40, 'Busia',40), (41, 'Siaya',41), (42, 'Kisumu',42), (43, 'Homa Bay',43), (44, 'Migori',44), (45, 'Kisii',45), (46, 'Nyamira',46), (47, 'Nairobi',47);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE `contact` (
+  `cid` int(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `message` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`cid`, `name`, `email`, `phone`, `subject`, `message`) VALUES
+(2, 'demo', 'demo@gmail.com', '9765989689', 'demo', 'demo'),
+(4, 'test', 'test@gmail.com', '7976976979', 'test', 'test'),
+(5, 'final', 'final@gmail.com', '7697967967', 'final', 'final'),
+(6, 'disha', 'disha@gmail.com', '7898797696', 'demo', 'demo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE `feedback` (
+  `fid` int(50) NOT NULL,
+  `uid` int(50) NOT NULL,
+  `fdescription` varchar(300) NOT NULL,
+  `status` int(1) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`fid`, `uid`, `fdescription`, `status`, `date`) VALUES
+(2, 15, 'Magicbricks made my life easy. It helped me with the search for my first ever investment i.e. 1BHK apartment in Mira Road. Thanks to the team for providing relevant tools like EMI calculator and smart search.\r\n', 1, '2020-04-03'),
+(3, 18, 'I am young professional, Magicbricks search helped me in shortlisting property in Navi Mumbai. I learned what kind of property will cost me how much and what are the types of amenities I will be getting?', 1, '2020-04-03'),
+(4, 21, 'I was looking for a flat in Andheri and Magicbricks website helped me get one smoothly. I could choose not just the property but also check what others had to say about the area. The site is simple and user friendly.\r\n', 1, '2020-04-03'),
+(5, 23, 'The constant touch through other true calls really surprised me.They sent their officer to get the photographs of my shop & promptly posted all the pics which helped me in getting the tenant fast. Hats off to Magicbricks.\r\n', 1, '2020-04-03'),
+(6, 24, 'I moved to Mumbai from Delhi early this year and I looked online for a suitable apartment for rent in areas near my workplace in Andheri. Thanks Magicbricks for giving me so many options with Travel Time search.\r\n', 0, '2020-04-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
+
+DROP TABLE IF EXISTS `property`;
+CREATE TABLE `property` (
+  `pid` int(50) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `pcontent` longtext NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `bhk` varchar(50) NOT NULL,
+  `stype` varchar(100) NOT NULL,
+  `bedroom` int(50) NOT NULL,
+  `bathroom` int(50) NOT NULL,
+  `balcony` int(50) NOT NULL,
+  `kitchen` int(50) NOT NULL,
+  `hall` int(50) NOT NULL,
+  `floor` varchar(50) NOT NULL,
+  `size` int(50) NOT NULL,
+  `price` int(50) NOT NULL,
+  `location` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `feature` longtext NOT NULL,
+  `pimage` varchar(300) NOT NULL,
+  `pimage1` varchar(300) NOT NULL,
+ 
+  `uid` int(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
+
+  `totalfloor` varchar(50) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `property`
+--
+
+INSERT INTO `property` (`pid`, `title`, `pcontent`, `type`, `bhk`, `stype`, `bedroom`, `bathroom`, `balcony`, `kitchen`, `hall`, `floor`, `size`, `price`, `location`, `city`, `state`, `feature`, `pimage`, `pimage1`, `uid`, `status`,  `totalfloor`, `date`) VALUES
+(11, 'final', '', 'office', '2 BHK', 'sale', 1, 2, 3, 4, 5, '3rd Floor', 4321, 897898, 'sabji market main road vapi', 'valsad', 'gujarat', '	', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', 15, 'sold out', '', '', '', '', '2020-04-03 00:28:14'),
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+DROP TABLE IF EXISTS `state`;
+CREATE TABLE `state` (
+  `sid` int(50) NOT NULL,
+  `sname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`sid`, `sname`) VALUES
+(1, 'Mombasa'),
+(2, 'Kwale'),
+(3, 'Kilifi'),
+(4, 'Tana River'),
+(5, 'Lamu'),
+(6, 'Taita Taveta'),
+(7, 'Garissa'),
+(8, 'Wajir'),
+(9, 'Mandera'),
+(10, 'Marsabit'),
+(11, 'Isiolo'),
+(12, 'Meru'),
+(13, 'Tharaka-Nithi'),
+(14, 'Embu'),
+(15, 'Kitui'),
+(16, 'Machakos'),
+(17, 'Makueni'),
+(18, 'Nyandarua'),
+(19, 'Nyeri'),
+(20, 'Kirinyaga'),
+(21, "Murang'a"),
+(22, 'Kiambu'),
+(23, 'Turkana'),
+(24, 'West Pokot'),
+(25, 'Samburu'),
+(26, 'Trans-nzoia'),
+(27, 'Uasin Gishu'),
+(28, 'Elgeyo Marakwet'),
+(29, 'Nandi'),
+(30, 'Baringo'),
+(31, 'Laikipia'),
+(32, 'Nakuru'),
+(33, 'Narok'),
+(34, 'Kajiado'),
+(35, 'Kericho'),
+(36, 'Bomet'),
+(37, 'Kakamega'),
+(38, 'Vihiga'),
+(39, 'Bungoma'),
+(40, 'Busia'),
+(41, 'Siaya'),
+(42, 'Kisumu'),
+(43, 'Homa Bay'),
+(44, 'Migori'),
+(45, 'Kisii'),
+(46, 'Nyamira'),
+(47, 'Nairobi');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `uid` int(50) NOT NULL,
+  `uname` varchar(100) NOT NULL,
+  `uemail` varchar(100) NOT NULL,
+  `uphone` varchar(20) NOT NULL,
+  `upass` varchar(50) NOT NULL,
+  `utype` varchar(50) NOT NULL,
+  `uimage` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`uid`, `uname`, `uemail`, `uphone`, `upass`, `utype`, `uimage`) VALUES
+(14, 'admin', 'admin@gmail.com', '9876543210', 'admin', 'user', '3.jpg'),
+(15, 'aryan', 'aryan@gmail.com', '9878678678', 'aryan', 'agent', '2.jpg'),
+(16, 'demo', 'demo@gmail.com', '7976976979', 'demo', 'user', '1.jpg'),
+(21, 'disha', 'disha@gmail.com', '7976956956', 'disha', 'agent', '2.jpg'),
+(22, 'disha', 'disha1@gmail.com', '8997897869', 'disha', 'agent', '1.jpg'),
+(23, 'testing', 'testing@gmail.com', '9869596597', 'testing', 'builder', '1.jpg'),
+(24, 'some', 'some@gmail.com', '9689698697', 'some', 'builder', '3.jpg'),
+(25, 'test', 'test12@gmail.com', '9798678969', 'test', 'builder', 'avatar-3.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about`
+--
+ALTER TABLE `about`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`aid`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`fid`);
+
+--
+-- Indexes for table `property`
+--
+ALTER TABLE `property`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `about`
+--
+ALTER TABLE `about`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `cid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `cid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `fid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `property`
+--
+ALTER TABLE `property`
+  MODIFY `pid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `sid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `uid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
